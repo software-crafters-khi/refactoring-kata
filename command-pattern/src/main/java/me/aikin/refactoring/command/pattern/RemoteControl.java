1d932f8e8b21bcf6f28383ef7923be2152bc1cd4
Jazib Hassan <jazib.hassan@careem.com>
2020-05-23 15:56:09 +0500

    Created a mapper for off function of RemoteControl

--- command-pattern/src/main/java/me/aikin/refactoring/command/pattern/RemoteControl.java
+++ command-pattern/src/main/java/me/aikin/refactoring/command/pattern/RemoteControl.java
@@ -5,35 +5,28 @@ import java.util.Map;
 import me.aikin.refactoring.command.pattern.remoteactions.*;
 
 public class RemoteControl {
-    private final TurnOffLight turnOffLight;
-    private final TurnOffCeilingFan turnOffCeilingFan;
-    private final TurnOffStereo turnOffStereo;
 
     private final Map<Integer, SlotCommand> onCommandMap;
+    private final Map<Integer, SlotCommand> offCommandMap;
 
     public RemoteControl(Light light, Ceiling ceiling, Stereo stereo) {
-
-        this.turnOffLight = new TurnOffLight(light);
-        this.turnOffCeilingFan = new TurnOffCeilingFan(ceiling);
-        this.turnOffStereo = new TurnOffStereo(stereo);
 
         this.onCommandMap= new HashMap<>();
         this.onCommandMap.put(1,new TurnOnLight(light));
         this.onCommandMap.put(2,new TurnOnCeilingFan(ceiling));
         this.onCommandMap.put(3,new TurnOnStereo(stereo));
-    }
 
+        this.offCommandMap = new HashMap<>();
+        this.offCommandMap.put(1,new TurnOffLight(light));
+        this.offCommandMap.put(2,new TurnOffCeilingFan(ceiling));
+        this.offCommandMap.put(3,new TurnOffStereo(stereo));
+    }
 
     public void on(int slot) {
         this.onCommandMap.get(slot).execute();
     }
 
     public void off(int slot) {
-        if (slot == 1)
-            turnOffLight.execute();
-        if (slot == 2)
-            turnOffCeilingFan.execute();
-        if (slot == 3)
-            turnOffStereo.execute();
+        this.offCommandMap.get(slot).execute();
     }
 }
